67f291b2d62f76aec9b5b0700bd094c97e6ef41d
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-12 00:37:22 -0400

    Added the skeleton of a GameManager class, holding the data and handling the ordering of logic for running the game loop.

--- Lumen/Lumen/GameManager.cs
+++ Lumen/Lumen/GameManager.cs
@@ -7,5 +7,15 @@ namespace Lumen
 {
     class GameManager
     {
+        public List<Player> Players { get; set; }
+        public List<Enemy>  Enemies { get; set; }
+        public List<Prop>   Props   { get; set; }
+
+        public GameManager()
+        {
+            Players = new List<Player>();
+            Enemies = new List<Enemy>();
+            Props = new List<Prop>();
+        }
     }
 }
