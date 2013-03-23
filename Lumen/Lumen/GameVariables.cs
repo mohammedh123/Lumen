c73c00a29e5e578a6c64e9eb4461f2404758ee1e
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-22 21:06:46 -0400

    Added a GameVariables class with public static variables.

---
+++ Lumen/Lumen/GameVariables.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace Lumen
+{
+    public class GameVariables
+    {
+        public static float PlayerSpeed = 100; //pixels per second
+        public static float PlayerCollisionRadius = 16.0f;
+        public static int PlayerInitialCandles = 3;
+        public static float PlayerLanternRadius = 256.0f; //16*sqrt(2)
+
+        public static float EnemyCollisionRadius = 16.0f;
+
+        public static float CandleInitialRadius = 100.0f;
+        public static float CandleFlickerPeriod = 0.2f;
+        public static float CandleFlickerAmount = 5.0f;
+    }
+}
