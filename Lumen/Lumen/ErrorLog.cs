9b73ff1593bcb3854bbbc65f0d808821982a3016
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-22 21:05:15 -0400

    Added an ErrorLog class to facilitate exceptional errors to an external log.

---
+++ Lumen/Lumen/ErrorLog.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace Lumen
+{
+    public class ErrorLog
+    {
+        public const string LogFileName = "log.txt";
+
+        public static void Log(string logMessage)
+        {
+            using (var w = File.AppendText(LogFileName)) {
+                w.Write(Environment.NewLine + "Log ({0} {1}): ", DateTime.Now.ToLongTimeString(),
+                        DateTime.Now.ToLongDateString());
+                w.WriteLine();
+                w.WriteLine("{0}", logMessage);
+                w.WriteLine();
+                w.WriteLine("\t-----");
+            }
+        }
+    }
+}
