bb4a9176a034afae6389e4d84b8571d10d6a88af
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-22 21:09:40 -0400

    The rest of the files I've updated.

--- Lumen/Lumen/GameDriver.cs
+++ Lumen/Lumen/GameDriver.cs
@@ -1,5 +1,8 @@
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using Lumen.Entities;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
@@ -12,17 +15,55 @@ namespace Lumen
         private SpriteBatch _spriteBatch;
 
         private GameManager _gameManager;
+        private LightManager _lightManager;
+
+        private RenderTarget2D _sceneRT;
+        private const int MaxPlayers = 4;
+
+        public static readonly Vector2 DisplayResolution = new Vector2(1024,768);
 
         public GameDriver()
         {
             _graphics = new GraphicsDeviceManager(this);
-            _gameManager = new GameManager();
-
             var graphicsOptions = new GraphicsOptions();
             graphicsOptions.ApplySettings(_graphics);
 
+            _gameManager = new GameManager(DisplayResolution);
+            _lightManager = new LightManager();
+
             Content.RootDirectory = "Content";
         }
+
+#if DEBUG
+        private void LoadVariables()
+        {
+
+            try
+            {
+                using (var streamReader = new StreamReader("variables.txt"))
+                {
+                    string line;
+
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        line = line.Trim();
+
+                        if (String.IsNullOrEmpty(line)) continue;
+
+                        var variableName = line.Substring(0, line.IndexOf(' ')).Trim();
+                        var variableValue = line.Substring(line.IndexOf('=') + 1).Trim();
+
+                        var actualVariable = typeof(GameVariables).GetField(variableName);
+                        actualVariable.SetValue(null, Convert.ChangeType(variableValue, actualVariable.FieldType));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                ErrorLog.Log("Error was encountered with exception:" + Environment.NewLine + e);
+            }
+        }
+#endif
 
         protected override void Initialize()
         {
@@ -34,6 +75,15 @@ namespace Lumen
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
             TextureManager.LoadContent(Content);
+            _lightManager.LoadContent(_graphics, GraphicsDevice, Content);
+
+            for (var i = PlayerIndex.One; i <= PlayerIndex.Four; i++ )
+            {
+                if (GamePad.GetState(i).IsConnected)
+                    _gameManager.AddPlayer(new Player("player", new Vector2(100, 50)), i);
+            }
+
+            _sceneRT = new RenderTarget2D(GraphicsDevice, (int)DisplayResolution.X, (int)DisplayResolution.Y);
         }
 
         protected override void UnloadContent()
@@ -47,14 +97,35 @@ namespace Lumen
                 Exit();
             }
 
+#if DEBUG
+            if(Keyboard.GetState().IsKeyDown(Keys.Back))
+                LoadVariables();
+#endif
+
+            InputManager.BeginUpdate();
+            _gameManager.Update(gameTime);
+            InputManager.EndUpdate();
             base.Update(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
-            GraphicsDevice.Clear(Color.Black);
-            
+            GraphicsDevice.SetRenderTarget(_sceneRT);
+            _gameManager.DrawScene(_spriteBatch);
+
+            _lightManager.DrawScene(_gameManager.Props.Where(p => p.PropType == PropTypeEnum.Candle).Cast<Candle>(), GraphicsDevice, _spriteBatch);
+
+            _spriteBatch.Begin();
+            DrawFullscreenQuad(_sceneRT, _spriteBatch);
+            _spriteBatch.End();
+            _lightManager.DrawLightDarkness(GraphicsDevice, _spriteBatch, _sceneRT);
+
             base.Draw(gameTime);
+        }
+
+        private void DrawFullscreenQuad(Texture2D tex, SpriteBatch sb)
+        {
+            sb.Draw(tex, new Rectangle(0, 0, (int)DisplayResolution.X, (int)DisplayResolution.Y), Color.White);
         }
     }
 }
--- Lumen/Lumen/GameManager.cs
+++ Lumen/Lumen/GameManager.cs
@@ -2,20 +2,139 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Lumen.Entities;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
 
 namespace Lumen
 {
     class GameManager
     {
-        public List<Player> Players { get; set; }
+        protected List<Player> Players { get; set; }
         public List<Enemy>  Enemies { get; set; }
         public List<Prop>   Props   { get; set; }
 
-        public GameManager()
+        private readonly Vector2 _gameResolution;
+
+        public GameManager(Vector2 gameResolution)
         {
             Players = new List<Player>();
             Enemies = new List<Enemy>();
             Props = new List<Prop>();
+
+            _gameResolution = gameResolution;
+        }
+
+        public void AddPlayer(Player p)
+        {
+            if (Players.Contains(p)) return;
+
+            Props.Add(new AttachedCandle("candle", p) { Radius = GameVariables.PlayerLanternRadius });
+            Players.Add(p);
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            var dt = (float)gameTime.ElapsedGameTime.TotalSeconds;
+
+            //iterate through all entities and update them
+            foreach(var player in Players)
+                player.Update(dt);
+
+            foreach(var enemy in Enemies)
+                enemy.Update(dt);
+
+            foreach(var prop in Props)
+                prop.Update(dt);
+
+            //all entities have proper state updated now, now check for the following types of interactions
+            // Player vs Prop
+            // Enemy vs Player
+
+            foreach(var player in Players) {
+                Prop interactingProp = null, collidingProp = null;
+
+                foreach (var prop in Props.Where(p => !(p is AttachedCandle)))
+                {
+                    if (Collider.Collides(player, prop)) {
+                        if (player.IsInteractingWithProp) {
+                               interactingProp = prop;
+                            prop.OnInteract(player);
+                        }
+
+                        collidingProp = prop;
+
+                        break;
+                    }
+                }
+                
+                // cases:
+                // interacting + candle ->
+                //      if not players candle, pick up, increase player candle count
+                //      else do nothing
+                // interacting/not interacting + wax -> increase radius...or something
+                // interacting + no prop -> place candle
+
+                if(!player.IsInteractingWithProp && collidingProp != null) {
+                    if(collidingProp.PropType == PropTypeEnum.Wax) {
+                        //increase candle radius...TODO
+                    }
+                }
+                else if (player.IsInteractingWithProp && interactingProp != null) 
+                {
+                    if(interactingProp.PropType == PropTypeEnum.Wax) {
+                        //for now, do nothing. maybe we do something later...
+                    }
+                    else if(interactingProp.PropType == PropTypeEnum.Candle) {
+                        var candleProp = interactingProp as Candle;
+
+                        //pick up candle, increase player's candle count
+                        player.NumCandlesLeft++;
+
+                        Props.Remove(candleProp);
+                    }
+                }
+                else if (player.IsInteractingWithProp && collidingProp == null && interactingProp == null) {
+                    //- place candle, if player has any candles left, place candle, decrement, otherwise nothing
+
+                    if (player.NumCandlesLeft > 0) {
+                        player.NumCandlesLeft--;
+                        Props.Add(new Candle("candle", player.Position, player));
+                    }
+                }
+            }
+        }
+
+        public void DrawScene(SpriteBatch sb)
+        {
+            sb.Begin();
+
+            DrawBackground(sb);
+
+            foreach (var player in Players)
+                player.Draw(sb);
+
+            foreach (var enemy in Enemies)
+                enemy.Draw(sb);
+
+            foreach (var prop in Props)
+                prop.Draw(sb);
+
+            sb.End();
+        }
+
+        public void DrawBackground(SpriteBatch sb)
+        {
+            //sb MUST have been Begin'd
+
+            sb.Draw(TextureManager.GetTexture("background"), new Rectangle(0, 0, (int)_gameResolution.X, (int)_gameResolution.Y), Color.White);
+        }
+
+        public void AddPlayer(Player player, PlayerIndex playerIndex)
+        {
+            AddPlayer(player);
+            Players.Last().PlayerNum = playerIndex;
         }
     }
 }
