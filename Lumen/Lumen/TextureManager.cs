f57ae00737890c6bc049919483e64d15a78faf60
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-22 21:05:39 -0400

    Updated TextureManager to also contain extra data for textures (origin).

--- Lumen/Lumen/TextureManager.cs
+++ Lumen/Lumen/TextureManager.cs
@@ -2,37 +2,62 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 
 namespace Lumen
 {
-    class TextureManager
+    public class TextureManager
     {
-        private static Dictionary<string, Texture2D> _textures;
+        public static readonly Vector2 PlayerOrigin = new Vector2(16,16);
+
+        private struct TextureDetails
+        {
+            public Texture2D Texture;
+            public Vector2 Origin;
+
+            public TextureDetails(Texture2D texture, Vector2 origin)
+            {
+                Texture = texture;
+                Origin = origin;
+            }
+        };
+
+        private static Dictionary<string, TextureDetails> _textureDetails;
  
         public static void LoadContent(ContentManager contentManager)
         {
-            _textures = new Dictionary<string, Texture2D>();
+            _textureDetails = new Dictionary<string, TextureDetails>();
         
-            LoadImages();
+            LoadTextureInformation(contentManager);
         }
-        
+
         public static Texture2D GetTexture(string name)
         {
-            Texture2D tex;
-            _textures.TryGetValue(name, out tex);
+            TextureDetails td;
+            if (_textureDetails.TryGetValue(name, out td))
+                return td.Texture;
 
-            if (tex != null)
-                return tex;
-
-            throw new ArgumentException(String.Format("A texture with the name {0} has not been added to the texture map yet.", name), "name");
+            throw new ArgumentException(String.Format("A texture with the name {0} has not been added to the texture details map yet.", name), "name");
         }
 
-        private static void LoadImages()
+        public static Vector2 GetOrigin(string name)
         {
-            //TODO: load images here
-            throw new NotImplementedException();
+            TextureDetails td;
+            if (_textureDetails.TryGetValue(name, out td))
+                return td.Origin;
+
+            throw new ArgumentException(String.Format("A texture origin with the name {0} has not been added to the texture details map yet.", name), "name");
+        }
+
+        private static void LoadTextureInformation(ContentManager contentManager)
+        {
+            _textureDetails.Add("player",   new TextureDetails(contentManager.Load<Texture2D>("player"),    new Vector2(16,16)));
+            _textureDetails.Add("enemy",    new TextureDetails(contentManager.Load<Texture2D>("enemy"),     new Vector2(16,16)));
+            _textureDetails.Add("wax", new TextureDetails(contentManager.Load<Texture2D>("wax"), new Vector2(16, 16)));
+            _textureDetails.Add("candle", new TextureDetails(contentManager.Load<Texture2D>("candle"), new Vector2(16, 16)));
+            _textureDetails.Add("background", new TextureDetails(contentManager.Load<Texture2D>("background"), new Vector2(512, 384)));
         }
     }
 }
