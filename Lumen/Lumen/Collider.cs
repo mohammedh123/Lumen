70a51a018b95e955c75ffbc9d676a8d970ae3266
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-22 21:06:11 -0400

    Added a static Collider class.

---
+++ Lumen/Lumen/Collider.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Lumen.Entities;
+using Microsoft.Xna.Framework;
+
+namespace Lumen
+{
+    public class Collider
+    {
+        internal static bool Collides(Player player, Prop prop)
+        {
+            return CirclesCollide(player.Position, GameVariables.PlayerCollisionRadius, prop.Position,
+                                  GameVariables.EnemyCollisionRadius);
+        }
+
+        private static bool CirclesCollide(Vector2 centerA, float radiusA, Vector2 centerB, float radiusB)
+        {
+            return (centerA - centerB).LengthSquared() < (radiusA + radiusB)*(radiusA + radiusB);
+        }
+    }
+}
