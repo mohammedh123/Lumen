ac135205bbb6e1244f54523d08f3af7fea22ba43
Mohammed Hossain <mohammedh123@gmail.com>
2013-03-30 01:14:54 -0400

    Added more collider methods to Collider (some are not implemented...).

--- Lumen/Lumen/Collider.cs
+++ Lumen/Lumen/Collider.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Lumen.Entities;
+using Lumen.Props;
 using Microsoft.Xna.Framework;
 
 namespace Lumen
@@ -13,6 +14,35 @@ namespace Lumen
         {
             return CirclesCollide(player.Position, GameVariables.PlayerCollisionRadius, prop.Position,
                                   GameVariables.EnemyCollisionRadius);
+        }
+
+        internal static bool Collides(Player player, Block block, bool useVelocity = false)
+        {
+            if(useVelocity)
+            {
+                
+            }
+            else
+            {
+                
+            }
+
+            return false;
+        }
+
+        internal static bool Collides(Player player, Player otherPlayer, bool useVelocityPlayerOne = false, bool useVelocityPlayerTwo = false)
+        {
+            var p1 = new Rectangle((int) (player.Position.X - GameVariables.PlayerCollisionRadius + (useVelocityPlayerOne ? player.Velocity.X : 0)),
+                                   (int)(player.Position.Y - GameVariables.PlayerCollisionRadius + (useVelocityPlayerOne ? player.Velocity.Y : 0)),
+                                   (int) (GameVariables.PlayerCollisionRadius*2),
+                                   (int) (GameVariables.PlayerCollisionRadius*2));
+
+            var p2 = new Rectangle((int)(otherPlayer.Position.X - GameVariables.PlayerCollisionRadius + (useVelocityPlayerTwo ? otherPlayer.Velocity.X : 0)),
+                                   (int)(otherPlayer.Position.Y - GameVariables.PlayerCollisionRadius + (useVelocityPlayerTwo ? otherPlayer.Velocity.Y : 0)),
+                                   (int) (GameVariables.PlayerCollisionRadius*2),
+                                   (int) (GameVariables.PlayerCollisionRadius*2));
+
+            return p1.Intersects(p2);
         }
 
         private static bool CirclesCollide(Vector2 centerA, float radiusA, Vector2 centerB, float radiusB)
