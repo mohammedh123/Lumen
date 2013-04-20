f449a2df95cfdd519ad93d0dc7e98291313337fd
Mohammed Hossain <mohammedh123@gmail.com>
2013-04-20 10:57:54 -0400

    Added a fade in/fade out to the blinking light.

--- Lumen/Lumen/Props/BlinkingLight.cs
+++ Lumen/Lumen/Props/BlinkingLight.cs
@@ -1,15 +1,43 @@
 ﻿using System;
 using Lumen.Entities;
+using Microsoft.Xna.Framework;
 
 namespace Lumen.Props
 {
     class BlinkingLight : Light
     {
+        private enum BlinkingLightFadeState
+        {
+            None,
+            FadingIn,
+            FadingOut
+        }
+
         public Entity Owner = null;
 
-        public float timer;
-        public float durationTimer;
-        private float _lightRadius;
+        private BlinkingLightFadeState _fadeState = BlinkingLightFadeState.None;
+        private float _timer, _durationTimer, _fadeTimer=-1.0f;
+        private readonly float _lightRadius;
+
+        private bool CanBeginBlink
+        {
+            get { return _timer == 0.0f; }
+        }
+
+        private bool IsLightOn
+        {
+            get { return _durationTimer > 0.0f; }
+        }
+
+        private bool IsDoneFadingIn
+        {
+            get { return _fadeTimer >= GameVariables.BlinkingFadeInDuration; }
+        }
+
+        private bool IsDoneFadingOut
+        {
+            get { return _fadeTimer >= GameVariables.BlinkingFadeOutDuration; }
+        }
 
         public BlinkingLight(string textureKeyName, Entity owner, float lightRadius) : base(textureKeyName, owner.Position, 0)
         {
@@ -23,23 +51,52 @@ namespace Lumen.Props
         {
             Position = Owner.Position;
             
-            if(timer == 0.0f)
+            if(CanBeginBlink) //begin fading in
             {
-                timer = GameVariables.BlinkingPeriod;
-                durationTimer = GameVariables.BlinkingDuration;
+                _timer = GameVariables.BlinkingPeriod;
+                _durationTimer = GameVariables.BlinkingDuration;
+                _fadeTimer = 0.0f;
+                _fadeState = BlinkingLightFadeState.FadingIn;
+            }
+            else if(_fadeState == BlinkingLightFadeState.FadingIn)
+            {
+                LightRadius = _lightRadius * MathHelper.SmoothStep(0.0f, 1.0f, _fadeTimer / GameVariables.BlinkingFadeInDuration);
+
+                _fadeTimer = Math.Min(_fadeTimer + dt, GameVariables.BlinkingFadeInDuration);
+
+                if(IsDoneFadingIn)
+                {
+                    _fadeState = BlinkingLightFadeState.None;
+                    _fadeTimer = 0.0f;
+                }
+            }
+            else if(_fadeState == BlinkingLightFadeState.None && IsLightOn)
+            {
+                LightRadius = IsLightOn ? _lightRadius : 0;
+
+                _durationTimer = Math.Max(_durationTimer - dt, 0);
+
+                if(!IsLightOn)
+                {
+                    _fadeState = BlinkingLightFadeState.FadingOut;
+                    _fadeTimer = 0.0f;
+                }
+
+            }
+            else if(_fadeState == BlinkingLightFadeState.FadingOut)
+            {
+                LightRadius = _lightRadius*MathHelper.SmoothStep(1.0f, 0.0f, _fadeTimer / GameVariables.BlinkingFadeOutDuration);
+
+                _fadeTimer = Math.Min(_fadeTimer + dt, GameVariables.BlinkingFadeOutDuration);
+
+                if (IsDoneFadingOut)
+                {
+                    _fadeState = BlinkingLightFadeState.None;
+                    _fadeTimer = -1.0f;
+                }
             }
 
-            if(durationTimer > 0)
-            {
-                LightRadius = _lightRadius;
-            }
-            else
-            {
-                LightRadius = 0;
-            }
-
-            timer = Math.Max(timer - dt, 0.0f);
-            durationTimer = Math.Max(durationTimer - dt, 0);
+            _timer = Math.Max(_timer - dt, 0.0f);
         }
     }
 }
