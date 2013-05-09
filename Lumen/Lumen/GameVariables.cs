01b965c1c14302f3266ff42d72ff92bf764dcf9e
Mohammed Hossain <mohammedh123@gmail.com>
2013-05-09 00:29:07 -0400

    Applied the variable changes.

--- Lumen/Lumen/GameVariables.cs
+++ Lumen/Lumen/GameVariables.cs
@@ -5,38 +5,38 @@ namespace Lumen
     public class GameVariables
     {
         public const int PlayerStartingHealth = 3; //CHANGIN THIS REQUIRES A CHANGE OF ART+CODE
-        public const float MaxScreenShake = 0.0f;
-        public static float PlayerSpeed = 100; //pixels per second
-        public static float PlayerCollisionRadius = 10.0f;
-        public static float PlayerOrbsDistance = 20.0f;
-        public static float PlayerOrbsPeriod = 0.5f;
-        public static float PlayerLightDuration = 1.0f;
-        public static float PlayerLightRadius = 150.0f;
-        public static float PlayerLightFadeInDuration = 0.3f;
-        public static float PlayerLightFadeOutDuration = 0.3f;
+        public const float MaxScreenShake = 0.3f;
+        public static float PlayerSpeed = 300; //pixels per second
+        public static float PlayerCollisionRadius = 16.0f;
+        public static float PlayerOrbsDistance = 15.0f;
+        public static float PlayerOrbsPeriod = 0.6f;
+        public static float PlayerLightDuration = 0.5f;
+        public static float PlayerLightRadius = 120.0f;
+        public static float PlayerLightFadeInDuration = 0.125f;
+        public static float PlayerLightFadeOutDuration = 0.125f;
         public static float PlayerHitVibrationDuration = 0.25f;
         public static float PlayerHitBlinkingDuration = 3.0f;
 
-        public static float EnemySpeed = 80.0f;
+        public static float EnemySpeed = 300.0f;
         public static float EnemyCollisionRadius = 24.0f;
-        public static float EnemyAttackCooldown = 3.0f;
-        public static float EnemyAttackMaxRadius = 512.0f;
-        public static float EnemyAttackRadiusGrowth = 200.0f;
-        public static float EnemyAttackRadiusAcceleration = 100.0f;
-        public static float EnemyLightRadiusWhileCharging = 64.0f;
-        public static float EnemyAttackTotalDuration = 1.00f;
-        public static float EnemyEnergyRegeneration = 20.0f;
-        public static float EnemySpeedWhileCharging = 30.0f;
+        public static float EnemyAttackCooldown = 1.0f;
+        public static float EnemyAttackMaxRadius = 80.0f;
+        public static float EnemyAttackRadiusGrowth = 160.0f;
+        public static float EnemyAttackRadiusAcceleration = 40.0f;
+        public static float EnemyLightRadiusWhileCharging = 20.0f;
+        public static float EnemyAttackTotalDuration = 0.067f;
+        public static float EnemyEnergyRegeneration = 2000.0f;
+        public static float EnemySpeedWhileCharging = 250.0f;
 
         public static bool IsScreenWrapping = false;
 
         public static float CameraZoom = 1.0f;
 
-        public static float BlinkingPeriod = 1.0f;
-        public static float BlinkingDuration = 0.1f;
-        public static float BlinkingRadius = 50.0f;
-        public static float EnemyBlinkingRadius = 50.0f;
-        public static float BlinkingFadeInDuration = 0.1f;
+        public static float BlinkingPeriod = 0.5f;
+        public static float BlinkingDuration = 0.625f;
+        public static float BlinkingRadius = 24.0f;
+        public static float EnemyBlinkingRadius = 4.0f;
+        public static float BlinkingFadeInDuration = 0.25f;
         public static float BlinkingFadeOutDuration = 0.1f;
         public static float BlinkingMaxFrequency = 16.0f;
 
